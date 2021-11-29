b7d64600e6e2578c686f7f4682b5630a7b16f694
beepsort <mh20198@essex.ac.uk>
2021-11-29 13:50:31 +0000

    setup script now installs all config symlinks

--- Code/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-ln -s $(pwd)/User/settings.json ~/.config/Code/User/settings.json
-ln -s $(pwd)/User/keybindings.json ~/.config/Code/User/keybindings.json
---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo Setting up doom emacs...
+ln -s $(pwd)/.doom.d ~/.doom.d
+
+echo Setting up VSCode...
+ln -s $(pwd)/Code/User/settings.json ~/.config/Code/User/settings.json
+ln -s $(pwd)/Code/User/keybindings.json ~/.config/Code/User/keybindings.json
+
+echo Done!
